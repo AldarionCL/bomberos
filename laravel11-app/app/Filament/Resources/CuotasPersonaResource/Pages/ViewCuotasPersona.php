a6ecb8a3121ffda86890b397aa177c914f46dcd3
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-12-30 17:42:47 -0300

    Filament update - Layout

---
+++ laravel11-app/app/Filament/Resources/CuotasPersonaResource/Pages/ViewCuotasPersona.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\CuotasPersonaResource\Pages;
+
+use App\Filament\Resources\CuotasPersonaResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewCuotasPersona extends ViewRecord
+{
+    protected static string $resource = CuotasPersonaResource::class;
+}
