48b86dc7bf5407e0acb546f88f3f17db65470d9f
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2025-01-09 18:16:10 -0300

    Filament update - route

--- laravel11-app/routes/web.php
+++ laravel11-app/routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-//    return redirect('/app');
-    return view('welcome');
+    return redirect('/app');
+//    return view('welcome');
 });
 
 Route::get('/sincCuotas', [\App\Http\Controllers\CuotasController::class, 'sincronizarCuotas'] );
