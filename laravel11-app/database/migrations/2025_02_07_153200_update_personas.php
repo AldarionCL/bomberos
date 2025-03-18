c78859407f38b96d769ddca3015f80e31f860c27
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2025-03-18 16:10:03 -0300

    Filament update

--- laravel11-app/database/migrations/2025_02_07_153200_update_personas.php
+++ laravel11-app/database/migrations/2025_02_07_153200_update_personas.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('personas', function (Blueprint $table) {
-            $table->string('NivelEstudio')->nullable();
             $table->string('LugarOcupacion')->nullable();
             $table->string('GrupoSanguineo')->nullable();
 
