f96f3b8ff9da6f09264ca2b25b516dfe8beace9e
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2025-05-23 15:46:57 -0400

    Dashboard y Notificaciones

--- laravel11-app/database/migrations/2025_05_11_000344_add_cuotas_tipo.php
+++ laravel11-app/database/migrations/2025_05_11_000344_add_cuotas_tipo.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Cuotas', function (Blueprint $table) {
+        Schema::table('cuotas', function (Blueprint $table) {
             $table->string('TipoCuota', 50)->nullable();
 
         });
