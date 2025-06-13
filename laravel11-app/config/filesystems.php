f8f3334fb4d58537d85016ee04d22fc292c74171
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2025-06-13 17:55:36 -0400

    Importacion de cuotas

--- laravel11-app/config/filesystems.php
+++ laravel11-app/config/filesystems.php
@@ -41,6 +41,16 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
             'visibility' => 'public',
             'throw' => false,
         ],
