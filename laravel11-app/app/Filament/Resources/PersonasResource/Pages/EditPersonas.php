e5bf2acd73172a054fa337281247ed900263f382
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2025-07-17 03:26:39 -0400

    filament reglas cuotas

--- laravel11-app/app/Filament/Resources/PersonasResource/Pages/EditPersonas.php
+++ laravel11-app/app/Filament/Resources/PersonasResource/Pages/EditPersonas.php
@@ -15,15 +15,7 @@ class EditPersonas extends EditRecord
         return [
             Actions\DeleteAction::make()
             ->before(function ($record) {
-                if($record->cuotas) {
-                    if($record->cuotas->count() > 0) {
-                        // If there are associated cuotas, prevent deletion
-                        return Actions\Action::make('error')
-                            ->label('No se puede eliminar, existen cuotas asociadas')
-                            ->color('danger');
-                    }
 
-                }
             }),
         ];
     }
