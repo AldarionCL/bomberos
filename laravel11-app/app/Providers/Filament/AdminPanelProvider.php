cb8c06d61789dcd3f7ba8574d3d7aff14782b8be
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2025-10-24 12:56:09 -0300

    ChangePassword

--- laravel11-app/app/Providers/Filament/AdminPanelProvider.php
+++ laravel11-app/app/Providers/Filament/AdminPanelProvider.php
@@ -12,6 +12,7 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -73,6 +74,8 @@ class AdminPanelProvider extends PanelProvider
             ->databaseNotifications()
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->profile()
+            ->maxContentWidth(MaxWidth::Full);
     }
 }
