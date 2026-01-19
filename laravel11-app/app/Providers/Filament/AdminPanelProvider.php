46fc73ab65ecf51ce6b9971709109e19ad571a3a
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2026-01-18 21:03:03 -0300

    Update

--- laravel11-app/app/Providers/Filament/AdminPanelProvider.php
+++ laravel11-app/app/Providers/Filament/AdminPanelProvider.php
@@ -75,6 +75,7 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
+            ->darkMode(false)
             ->profile()
             ->maxContentWidth(MaxWidth::Full);
     }
