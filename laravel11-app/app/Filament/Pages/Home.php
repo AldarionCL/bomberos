002901057121a8973905f9b8be12759247b8a2fc
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2026-06-11 18:07:54 -0400

    Update Home page layout and enhance noticias display with featured post and improved styling

--- laravel11-app/app/Filament/Pages/Home.php
+++ laravel11-app/app/Filament/Pages/Home.php
@@ -6,10 +6,10 @@ use Filament\Pages\Page;
 
 class Home extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-home';
-    protected static ?string $navigationLabel = 'Inicio';
-    protected static ?string $title = 'Inicio';
-    protected static ?int $navigationSort = -10;
+    protected static ?string $navigationIcon = 'heroicon-o-newspaper';
+    protected static ?string $navigationLabel = 'Noticias';
+    protected static ?string $title = 'Noticias';
+    protected static ?int $navigationSort = -1;
 
     protected static string $view = 'filament.pages.home';
 }
